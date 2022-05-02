c7b5cca13a7be613a98faf8b55486192fc6a4b29
xzifan <xiao@zifan.dev>
2022-05-02 14:40:49 +0800

    replace react

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 // important: set the root element with id 
+// @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('react-micro-app')!);
 root.render(
   <ChakraProvider>
